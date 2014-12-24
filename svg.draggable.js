7b1a1f57e530caea8ded1d55d7507af5de3f2c52
Ionică Bizău <bizauionica@yahoo.com>
2014-12-24 15:38:19 +0200

    Fixed semicolons and other syntax

--- svg.draggable.js
+++ svg.draggable.js
@@ -7,39 +7,40 @@
             var start, drag, end, element = this;
 
             /* remove draggable if already present */
-            if (typeof this.fixed === 'function')
+            if (typeof this.fixed === 'function') {
                 this.fixed();
+            }
 
             /* ensure constraint object */
-            constraint = constraint || {}
+            constraint = constraint || {};
 
             /* start dragging */
             start = function(event) {
-                var parent = this.parent._parent(SVG.Nested) || this._parent(SVG.Doc)
-                event = event || window.event
+                var parent = this.parent._parent(SVG.Nested) || this._parent(SVG.Doc);
+                event = event || window.event;
 
                 /* invoke any callbacks */
-                if (element.beforedrag)
-                    element.beforedrag(event)
+                if (element.beforedrag) {
+                    element.beforedrag(event);
+                }
 
                 /* get element bounding box */
-                var box = element.bbox()
+                var box = element.bbox();
 
                 if (element instanceof SVG.G) {
-                    box.x = element.x()
-                    box.y = element.y()
-
+                    box.x = element.x();
+                    box.y = element.y();
                 } else if (element instanceof SVG.Nested) {
                     box = {
                         x: element.x(),
                         y: element.y(),
                         width: element.width(),
                         height: element.height()
-                    }
+                    };
                 }
 
                 /* store event */
-                element.startEvent = event
+                element.startEvent = event;
 
                 /* store start position */
                 element.startPosition = {
@@ -49,27 +50,33 @@
                     height: box.height,
                     zoom: parent.viewbox().zoom,
                     rotation: element.transform('rotation') * Math.PI / 180
-                }
+                };
 
                 /* add while and end events to window */
-                SVG.on(window, 'mousemove', drag)
-                SVG.on(window, 'mouseup', end)
+                SVG.on(window, 'mousemove', drag);
+                SVG.on(window, 'mouseup', end);
 
                 /* invoke any callbacks */
-                if (element.dragstart)
+                if (element.dragstart) {
                     element.dragstart({
                         x: 0,
                         y: 0,
                         zoom: element.startPosition.zoom
-                    }, event)
+                    }, event);
+                }
 
                 /* prevent selection dragging */
-                event.preventDefault ? event.preventDefault() : event.returnValue = false
+                if (event.preventDefault) {
+                    event.preventDefault();
+                } else {
+                    event.returnValue = false;
+                }
+
             };
 
             /* while dragging */
             drag = function(event) {
-                event = event || window.event
+                event = event || window.event;
 
                 if (element.startEvent) {
                     /* calculate move position */
@@ -80,87 +87,91 @@
                             x: event.pageX - element.startEvent.pageX,
                             y: event.pageY - element.startEvent.pageY,
                             zoom: element.startPosition.zoom
-                        }
+                        };
 
                     /* caculate new position [with rotation correction] */
-                    x = element.startPosition.x + (delta.x * Math.cos(rotation) + delta.y * Math.sin(rotation)) / element.startPosition.zoom
-                    y = element.startPosition.y + (delta.y * Math.cos(rotation) + delta.x * Math.sin(-rotation)) / element.startPosition.zoom
+                    x = element.startPosition.x + (delta.x * Math.cos(rotation) + delta.y * Math.sin(rotation)) / element.startPosition.zoom;
+                    y = element.startPosition.y + (delta.y * Math.cos(rotation) + delta.x * Math.sin(-rotation)) / element.startPosition.zoom;
 
                     /* move the element to its new position, if possible by constraint */
                     if (typeof constraint === 'function') {
-                        var coord = constraint(x, y)
+                        var coord = constraint(x, y);
 
                         if (typeof coord === 'object') {
-                            if (typeof coord.x != 'boolean' || coord.x)
-                                element.x(typeof coord.x === 'number' ? coord.x : x)
-                            if (typeof coord.y != 'boolean' || coord.y)
-                                element.y(typeof coord.y === 'number' ? coord.y : y)
-
+                            if (typeof coord.x !== 'boolean' || coord.x) {
+                                element.x(typeof coord.x === 'number' ? coord.x : x);
+                            } else if (typeof coord.y !== 'boolean' || coord.y) {
+                                element.y(typeof coord.y === 'number' ? coord.y : y);
+                            }
                         } else if (typeof coord === 'boolean' && coord) {
-                            element.move(x, y)
+                            element.move(x, y);
                         }
 
                     } else if (typeof constraint === 'object') {
                         /* keep element within constrained box */
-                        if (constraint.minX != null && x < constraint.minX)
-                            x = constraint.minX
-                        else if (constraint.maxX != null && x > constraint.maxX - width)
-                            x = constraint.maxX - width
+                        if (constraint.minX !== null && x < constraint.minX) {
+                            x = constraint.minX;
+                        } else if (constraint.maxX !== null && x > constraint.maxX - width) {
+                            x = constraint.maxX - width;
+                        }
 
-                        if (constraint.minY != null && y < constraint.minY)
-                            y = constraint.minY
-                        else if (constraint.maxY != null && y > constraint.maxY - height)
-                            y = constraint.maxY - height
+                        if (constraint.minY !== null && y < constraint.minY) {
+                            y = constraint.minY;
+                        } else if (constraint.maxY !== null && y > constraint.maxY - height) {
+                            y = constraint.maxY - height;
+                        }
 
-                        element.move(x, y)
+                        element.move(x, y);
                     }
 
                     /* invoke any callbacks */
-                    if (element.dragmove)
-                        element.dragmove(delta, event)
+                    if (element.dragmove) {
+                        element.dragmove(delta, event);
+                    }
                 }
-            }
+            };
 
             /* when dragging ends */
             end = function(event) {
-                event = event || window.event
+                event = event || window.event;
 
                 /* calculate move position */
                 var delta = {
                     x: event.pageX - element.startEvent.pageX,
                     y: event.pageY - element.startEvent.pageY,
                     zoom: element.startPosition.zoom
-                }
+                };
 
                 /* reset store */
-                element.startEvent = null
-                element.startPosition = null
+                element.startEvent = null;
+                element.startPosition = null;
 
                 /* remove while and end events to window */
-                SVG.off(window, 'mousemove', drag)
-                SVG.off(window, 'mouseup', end)
+                SVG.off(window, 'mousemove', drag);
+                SVG.off(window, 'mouseup', end);
 
                 /* invoke any callbacks */
-                if (element.dragend)
-                    element.dragend(delta, event)
-            }
+                if (element.dragend) {
+                    element.dragend(delta, event);
+                }
+            };
 
             /* bind mousedown event */
-            element.on('mousedown', start)
+            element.on('mousedown', start);
 
             /* disable draggable */
             element.fixed = function() {
-                element.off('mousedown', start)
+                element.off('mousedown', start);
 
-                SVG.off(window, 'mousemove', drag)
-                SVG.off(window, 'mouseup', end)
+                SVG.off(window, 'mousemove', drag);
+                SVG.off(window, 'mouseup', end);
 
-                start = drag = end = null
+                start = drag = end = null;
 
-                return element
-            }
+                return element;
+            };
 
-            return this
+            return this;
         }
-    })
+    });
 }).call(this);
